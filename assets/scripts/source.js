142d7f697a0218f09a6252637d201525c6efe89f
Ahmed Elsayed Elbaz <ahmedelsayede75@gmail.com>
2025-11-21 18:06:36 +0200

    feat: add company page

--- assets/scripts/source.js
+++ assets/scripts/source.js
@@ -1,10 +1,32 @@
+// Get current page name from URL
+const currentPage =
+  window.location.pathname.split("/").pop().split(".")[0] || "";
+
+// Select all nav links
+const navLinks = document.querySelectorAll(".nav-item a");
+
+navLinks.forEach((link) => {
+  const linkPage = link.getAttribute("href").split("/").pop().split(".")[0];
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
+
 gsap.to(".testimonials__bottom__body", {
   xPercent: -50,
   ease: "none",
   duration: 60,
   repeat: -1,
 });
+
 gsap.to(".scroll-bar__list", {
+  xPercent: -50,
+  ease: "none",
+  duration: 30,
+  repeat: -1,
+});
+gsap.to(".test-scroll .list", {
   xPercent: -50,
   ease: "none",
   duration: 30,
